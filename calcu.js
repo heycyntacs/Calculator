35bbf9c915779ad687ae982516d5c08b2e4bb1bb
Jiggy Brillantes <brillantes.git@gmail.com>
2021-05-08 21:16:47 +0800

    Fixed Operations. Error Checking Left

--- calcu.js
+++ calcu.js
@@ -3,10 +3,10 @@ const inputDecimal = document.querySelector('.inputDecimal');
 const screen = document.querySelector('.screen');
 const clear = document.querySelector('.clear');
 const del = document.querySelector('.delete');
+const equals = document.querySelector('.equals');
 const operators = document.querySelectorAll('.operator');
 let operandA, operandB;
 let operatorValue = '';
-let inputReset = false;
 
 //FOR INPUTTING NUMBERS
 //Buttons
@@ -14,9 +14,9 @@ inputButtons.forEach(button => button.addEventListener('click', () => input(butt
 inputDecimal.addEventListener('click', decimal);
 clear.addEventListener('click', reset);
 del.addEventListener('click', backSpace);
+equals.addEventListener('click', eval);
 
 function input(button) {
-    if (inputReset === true) screen.textContent = '';
     screen.textContent += (button);
 }
 
@@ -28,11 +28,14 @@ function decimal () {
 
 function reset () {
     screen.textContent = '';
+    operandA = 0;
+    operandB = 0;
 }
 
 function backSpace () {
     screen.textContent = screen.textContent.slice(0, - 1);
 }
+
 
 //Keyboard-support
 window.addEventListener('keydown', e => {
@@ -41,6 +44,7 @@ window.addEventListener('keydown', e => {
     else if (e.key === 'Backspace') backSpace();
     else if (e.key === 'Escape') reset();
     else if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') setOperator(e.key);
+    else if (e.key === 'Enter') eval();
 })
 
 //FOR OPERATIONS
@@ -50,15 +54,14 @@ function setOperator(operator) {
     if(operatorValue !== '') eval();
     operandA = parseFloat(screen.textContent);
     operatorValue = operator;
-    inputReset = true;
+    screen.textContent = '';
 }
 
 function eval() {
-    if (operatorValue === '') return;
     operandB = parseFloat(screen.textContent);
+    if (isNaN(operandA) || isNaN(operandB)) return;
     if (operatorValue === '+' || operatorValue === '-') screen.textContent = operateA(operatorValue, operandA, operandB);
     else if (operatorValue === '*' || operatorValue === '/') screen.textContent = operateB(operatorValue, operandA, operandB);
-    operatorValue = '';
     console.log(operandA, operatorValue, operandB);
     console.log(screen.textContent);
 }
